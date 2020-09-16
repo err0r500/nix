f873104b2da22c90e6af51817682bce4ebce7c14
Matthieu JACQUOT <matthieu.jacquot.tech@protonmail.ch>
2020-09-16 13:47:28 +0000

    add bundles to default

--- default.nix
+++ default.nix
@@ -2,4 +2,8 @@ nixPkgs:
 {
   terraform_0_13_2 = import ./pkgs/terraform-0.13.2.nix nixPkgs;
   elm_0_19_1 = import ./pkgs/elm-0.19.1.nix nixPkgs;
+
+  bundles = {
+    elm = import ./bundles/elm.nix;
+  };
 }
